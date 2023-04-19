96e0de95434f789c2176b08e055803de0df6811a
sabbath54 <lukasz.marszalek320@gmail.com>
2023-04-19 15:38:47 +0200

    All functionalities added, app works as intended

--- dashboard.py
+++ dashboard.py
@@ -15,35 +15,44 @@ st.sidebar.header("Filter here:")
 seniority = st.sidebar.multiselect(
     "Select seniority:",
     options=df["seniority"].unique(),
+    default=df["seniority"].unique()
 )
 
 categorie = st.sidebar.multiselect(
     "Select categorie:",
     options=df["categories"].unique(),
+    default=df["categories"].unique()
 )
 
 df_selection = df.query(
     "seniority == @seniority & categories == @categorie"
-)
+).reset_index(drop=True)
 
 # Count the frequency of each skill required for the selected categories and seniority level
 skills_count = pd.Series([skill for skills in df_selection["required_skills"] for skill in eval(skills)]).value_counts()
 
-# Create a pie chart using Plotly Express to visualize the distribution of skills required
-fig = px.pie(skills_count, values=skills_count.values, names=skills_count.index, title="Distribution of Required Skills")
+# Create a horizontal bar chart using Plotly Express to visualize the top 15 required skills
+top_skills_count = skills_count.nlargest(15)[::-1]
+fig = px.bar(top_skills_count, x=top_skills_count.values, y=top_skills_count.index, orientation='h', title="Top 15 Required Skills")
+fig.update_layout(xaxis_title="Appearance count", yaxis_title="Skill name")
 
 # ---- MAINPAGE ----
 st.title(":bar_chart: What skills do I need to get a job?")
-st.markdown("##")
 
 # Display the total number of unique skills required for the selected categories and seniority level
 st.write("Total number of unique skills required for the selected categories and seniority level:", len(skills_count))
 
-# Display the pie chart
+# Display the bar chart
 st.plotly_chart(fig)
 
-# Display results
-st.write("Skill count:")
-st.dataframe(skills_count)
-st.write("Jobs:")
-st.dataframe(df_selection, use_container_width=True)
+# Display top 16-50 skills
+top_16_to_50_skills = skills_count.nlargest(50)[15:][::-1].reset_index().rename(columns={"index": "Skill name", 0: "Appearance count"})
+top_16_to_50_skills.index = range(16, len(top_16_to_50_skills) + 16)
+st.write("**Top 16-50 skills and their appearance count:**")
+st.dataframe(top_16_to_50_skills, height=500)
+
+# Display jobs
+st.write("**Jobs:**")
+df_selection.rename(columns={"listing_name": "Listing name", "categories": "Categorie", "seniority": "Seniority", "required_skills": "Required skills"}, inplace=True)
+df_selection.index += 1
+st.dataframe(df_selection)
